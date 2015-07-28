a1c05525c082a56da19e318543f46ddfcd938c66
Lucas van Walstijn <lvanwalstijn@virtualcitysystems.de>
2015-07-28 14:26:16 +0200

    clean up tms samples

--- examples/script.js
+++
@@ -1,8 +0,0 @@
-require({
-    baseUrl : '.',
-    paths : {
-        domReady : '../../Cesium/ThirdParty/requirejs-2.1.9/domReady',
-        Cesium : '../../Cesium/Source'
-    }
-}, ['script2'], function() {
-});
