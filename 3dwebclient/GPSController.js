4359b2c599465fb7209c61619133c1f95018b8d7
n9s2cya <n9s2cya@hotmail.com>
2017-06-13 20:45:45 +0200

    Added orientation support for iPhone.

--- 3dwebclient/GPSController.js
+++ 3dwebclient/GPSController.js
@@ -79,9 +79,9 @@
                 }
                 // alpha is in [0,360) degrees, while heading is [-pi,pi), where positive points eastward
                 if (oriAlpha < 180) {
-                    oriAlpha = Cesium.Math.toRadians(-ori.alpha);
+                    oriAlpha = Cesium.Math.toRadians(-oriAlpha);
                 } else {
-                    oriAlpha = Cesium.Math.toRadians(180 - (ori.alpha - 180));
+                    oriAlpha = Cesium.Math.toRadians(180 - (oriAlpha - 180));
                 }
 
                 // BETA
