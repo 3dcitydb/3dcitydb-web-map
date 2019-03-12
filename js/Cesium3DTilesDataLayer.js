73c7c84b27f1f92ef8dae35d62f159737d89cb74
Son-HNguyen <son.nguyen@tum.de>
2019-03-12 12:02:18 +0100

    Fix point cloud size

--- js/Cesium3DTilesDataLayer.js
+++ js/Cesium3DTilesDataLayer.js
@@ -278,7 +278,7 @@
         var timer = null;
         this._tileset.tileVisible.addEventListener(function (tile) {
             if (tile._content instanceof Cesium.PointCloud3DTileContent) {
-                tile._content._pointSize = 3;
+                tile._content._pointCloud._pointSize = 3;
             }
             if (!(tile._content instanceof Cesium.Batched3DModel3DTileContent))
                 return;
